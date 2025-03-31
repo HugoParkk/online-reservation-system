d1109a442a9ce9fc9bcd1c79ca27e2514470c1bc
hugo.p <jongyeonpark.dev@gmail.com>
2025-03-31 21:39:06 +0900

    project structure setting

---
+++ src/test/kotlin/com/hugopark/onlineReservationSystem/OnlineReservationSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hugopark.onlineReservationSystem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OnlineReservationSystemApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
